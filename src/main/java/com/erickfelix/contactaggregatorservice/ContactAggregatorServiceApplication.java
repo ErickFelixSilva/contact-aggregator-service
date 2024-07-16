28b61280ec0220ee1ce2a48528f9e88b8c2c671c
Erick Felix da Silva <erickfelix16@gmail.com>
2024-07-16 00:50:18 -0300

    Starting the project in spring initializr, with Spring Web, Lombok and OpenFeign

---
+++ src/main/java/com/erickfelix/contactaggregatorservice/ContactAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erickfelix.contactaggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactAggregatorServiceApplication.class, args);
+	}
+
+}
